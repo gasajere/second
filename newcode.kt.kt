4657ce3605802c7954dad412144bbf1d3ceb9d83
terence <gasajere@gmail.com>
2021-06-16 10:15:10 -0700

    {file was missing,had to push again}

---
+++ newcode.kt.kt
@@ -0,0 +1,14 @@
+package `if statements`
+
+//IF statements (a)
+fun main() {
+    val num1 = 24
+    val num2 = 12
+
+    if (num1 >= 12) {
+        val result = num1 / num2
+        print("Result is $result")
+    } else {
+        print("Zero decimals not allowed")
+    }
+}
